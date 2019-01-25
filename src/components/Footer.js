af29912d638ef5c7efef3ceb5640a0db92fe2c88
Zonayed Ahmed <zonayedpca@yahoo.com>
2019-01-25 19:01:44 +0530

    Footer Done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,11 +3,26 @@ import React from 'react';
 export default () => (
   <footer className="footer-area">
     <div className="footer">
-      <div className="left">
-        <p>This is a real example of react app</p>
+      <ul>
+        <li className="title">সম্পর্কেঃ</li>
+        <li><p>প্রোজেক্টটি সম্পূর্ণ সোর্স কোডসহ <a target="_blank" rel="noopener noreferrer" href="https://github.com/zonayedpca/react.zonayed.me">গিটহাবে</a> রয়েছে। আপনার ভালো লেগে থাকলে স্টার দিয়ে আসবেন। আপনার পরামর্শ, মন্তব্য এবং ভুলত্রুটি গিটহাবে ইস্যু করে দেওয়ার জন্যে অনুরোধ থাকলো</p></li>
+      </ul>
+      <div className="resource">
+        <ul>
+          <li className="title">আপনার জন্যেঃ</li>
+          <li><a target="_blank" rel="noopener noreferrer" href="https://js.zonayed.me">জাভাস্ক্রিপ্ট শিখুন</a></li>
+          <li><a target="_blank" rel="noopener noreferrer" href="https://medium.com/@zonayedpca">আমি মিডিয়ামে</a></li>
+          <li><a target="_blank" rel="noopener noreferrer" href="https://with.zonayed.me/bn">আমার ব্লগ</a></li>
+        </ul>
+        <ul>
+          <li className="title">রিসোর্সঃ</li>
+          <li><a target="_blank" rel="noopener noreferrer" href="https://js.zonayed.me">অফিশিয়াল ডকুমেন্টেশন</a></li>
+          <li><a target="_blank" rel="noopener noreferrer" href="https://medium.com/@zonayedpca">ইউডেমি কোর্স</a></li>
+          <li><a target="_blank" rel="noopener noreferrer" href="https://with.zonayed.me/bn">রিডাক্স ডকুমেন্টেশন</a></li>
+        </ul>
       </div>
-      <div className="right">
-        <p>By Zonayed Ahmed</p>
+      <div className="credit">
+        <p><span className="love">❤</span> এর সাথে ডেভেলপ করেছে <a target="_blank" rel="noopener noreferrer" href="https://www.zonayed.me">জুনায়েদ আহমেদ</a></p>
       </div>
     </div>
   </footer>
--- src/components/Home/SearchFavArea.js
+++ src/components/Home/SearchFavArea.js
@@ -7,7 +7,9 @@ import SearchCard from './SearchCard';
 const SearchFavArea = ({ searchVisibility, search, favorite }) => (
   <section className="search-fav-area">
     <ul>
-      {searchVisibility ? (search && (search.length <= 0 ? <p>নতুন টপিক সাজেস্ট করুন</p> : (search.map(({ id, title }) => <SearchCard key={id} id={id} title={title} />)))) : (favorite.length > 0 ? (favorite.map(({id, title}) => <FavCard key={id} id={id} title={title} />)) : 'Nothing')}
+      {searchVisibility ? (search && (search.length <= 0 ?
+        <div class="no-result"><a target="_blank" rel="noopener noreferrer" href="https://www.github.com/zonayedpca/react.zonayed.me" className="btn-suggest">নতুন টপিক সাজেস্ট করুন</a></div>
+         : (search.map(({ id, title }) => <SearchCard key={id} id={id} title={title} />)))) : (favorite.length > 0 ? (favorite.map(({id, title}) => <FavCard key={id} id={id} title={title} />)) : 'Nothing')}
     </ul>
   </section>
 )
