310a1c50e8bab2453d2bb2061ee9a28dacf97079
Zonayed Ahmed <zonayedpca@yahoo.com>
2019-01-30 20:37:40 +0530

    nav link changed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react';
 import logo from '../assets/logo.png';
 
 export default () => (
-  <a href="http://localhost:3000" className="brand">
+  <a href="https://react.zonayed.me" className="brand">
     <img className="logo" alt="রিঅ্যাক্ট বাংলা" src={logo} />
     <span>রিঅ্যাক্ট বাংলা</span>
   </a>
