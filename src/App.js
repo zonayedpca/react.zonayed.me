284ea362385afd116ec67cd7ef64004dbed4a16a
Zonayed Ahmed <zonayedpca@yahoo.com>
2019-01-31 05:04:31 +0000

    type of link added

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
       <Router>
         <div className="App">
           <Route exact path="/" render={() => <Home />} />
-          <Route exact path="/post/:id" render={(props) => <Post {...props} />} />
+          <Route exact path="/:type/:id" render={(props) => <Post {...props} />} />
           <Footer />
         </div>
       </Router>
