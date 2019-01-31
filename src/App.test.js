3ab09eaba9b12c5014327df0d046f7ffd4feefc9
Zonayed Ahmed <zonayedpca@yahoo.com>
2019-01-31 05:20:53 +0000

    test added

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import reduxPromise from 'redux-promise';
+
 import App from './App';
+
+import reducers from './reducers';
+
+const store = createStore(reducers, applyMiddleware(reduxPromise));
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Provider store={store}>
+    <App />
+  </Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
