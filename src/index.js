53f0af45e12d1e75e705f460a1078be9937ef826
Zonayed Ahmed <zonayedpca@yahoo.com>
2019-03-04 21:38:22 +0530

    reat snap added

--- src/index.js
+++ src/index.js
@@ -13,11 +13,19 @@ import * as serviceWorker from './serviceWorker';
 
 const store = createStore(reducers, applyMiddleware(reduxPromise));
 
-ReactDOM.render(
+const rootElement = document.getElementById("root");
+
+if (rootElement.hasChildNodes()) {
+  ReactDOM.hydrate(
   <Provider store={store}>
     <App />
-  </Provider>
-  , document.getElementById('root'));
+  </Provider>, rootElement);
+} else {
+  ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, rootElement);
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
